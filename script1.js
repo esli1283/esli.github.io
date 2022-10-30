c6eab11b768241651698ecc931df522d6df84071
Esli Lopez <esli_aaron@hotmail.com>
2022-10-30 18:56:03 -0500

    Mac

--- script1.js
+++ script1.js
@@ -1,47 +1,48 @@
 let today = new Date();
 
-function Job(company, start, end){
-    this.company = company;
-    this.startDate = start;
-    this.endDate = end;
-    this.duration = [];
+class Job {
+    constructor(company, start, end) {
+        this.company = company;
+        this.startDate = start;
+        this.endDate = end;
+        this.duration = [];
+    }
+    getJobDuration() {
+        let jobTotalTime = (this.endDate.getMonth() - this.startDate.getMonth() + (12 * (this.endDate.getFullYear() - this.startDate.getFullYear())) + 1);
+        let jobTimeYears = (jobTotalTime / 12);
+        let jobTimeMonths = (jobTotalTime % 12);
+        let jobTime;
+
+        if (Math.trunc(jobTimeYears) === 0) {
+            jobTime = (jobTimeMonths + " Months");
+        } else {
+            if (jobTimeMonths > 1) {
+                jobTime = (Math.trunc(jobTimeYears) + " Years " + jobTimeMonths + " Months");
+            }
+            else {
+                jobTime = (Math.trunc(jobTimeYears) + " Years " + jobTimeMonths + " Month");
+            }
+        };
+
+        if (jobTimeYears === 1) {
+            jobTime = (jobTimeYears + " Year");
+        };
+        return jobTime;
+    }
 };
 
-Job.prototype.getJobDuration = function(){
-    let jobTotalTime = (this.endDate.getMonth() - this.startDate.getMonth() + (12 * (this.endDate.getFullYear() - this.startDate.getFullYear())) + 1 )
-    let jobTimeYears = (jobTotalTime / 12);
-    let jobTimeMonths = (jobTotalTime % 12);
-    let jobTime;
-
-    if (Math.trunc(jobTimeYears)=== 0){
-        jobTime = (jobTimeMonths + " Months");
-    }else {
-        if (jobTimeMonths > 1 ){
-        jobTime = (Math.trunc(jobTimeYears) + " Years " + jobTimeMonths + " Months");
-        }
-        else {
-            jobTime = (Math.trunc(jobTimeYears) + " Years " + jobTimeMonths + " Month");
-        }
-    };
-
-    if (jobTimeYears === 1 ) {
-        jobTime = (jobTimeYears + " Year");
-    };
-    return jobTime;
-};
 
 
-
-let signatureTime = new Job("Signature", new Date(2018, 10, 01), new Date(2019,10,31));
+let USAATime = new Job ("USAA", new Date(2019,11,02), new Date());
+let signatureTime = new Job("Signature", new Date(2018, 11, 01), new Date(2019,10,31));
 let wellsFargoTime = new Job("Wells Fargo", new Date(2016, 10 , 01), new Date(2018,10,01));
 let goNetTime = new Job("goNet", new Date(2016,01,01), new Date(2016,10,01));
 let indraUSATime = new Job("Indra USA", new Date(2012,03,23), new Date(2015,11,31));
 let tataTime = new Job("TCS", new Date(2011,08,01), new Date(2012,03,01));
 let accentureTime = new Job("Accenture", new Date(2009,00,01), new Date(2011,08,30));
-signatureTime.duration.push(signatureTime.getJobDuration());
 
 
-
+document.getElementById("USAATime").innerHTML = USAATime.getJobDuration();
 document.getElementById("signatureTime").innerHTML = signatureTime.getJobDuration();
 document.getElementById("wellsFargoTime").innerHTML = wellsFargoTime.getJobDuration();
 document.getElementById("goNetTime").innerHTML = goNetTime.getJobDuration();
